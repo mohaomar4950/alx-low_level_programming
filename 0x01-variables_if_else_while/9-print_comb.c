c649119d021003a558154ce639c9e6ab1cb419f7
Mohammad Omar <mohaomar495@gmail.com>
2022-09-17 15:04:42 +0300

    combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,15 @@ int main(void)
 {
 	int n = 48;
 
-	while (n < 57)
+	while (n <= 57)
 	{
 		putchar(n);
-		putchar(',');
-		putchar(' ');
+		if (n != 57)
+		{
+
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
 	}
 	putchar(n);
