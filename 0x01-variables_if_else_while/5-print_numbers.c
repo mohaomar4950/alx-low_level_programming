e999bad61eda496107d084fb8d479f6a93020645
Mohammad Omar <mohaomar495@gmail.com>
2022-09-17 12:50:01 +0300

    printing numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+/**
+ * main - prints all numbers of base 10.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
