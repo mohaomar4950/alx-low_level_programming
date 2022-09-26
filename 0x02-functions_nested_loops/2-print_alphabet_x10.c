086eb8cc39e314871c4fb3e7ede92a64bf5c61e3
Mohammad Omar <mohaomar495@gmail.com>
2022-09-26 17:31:09 +0300

    alphabets ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,11 +13,11 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	}
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
 	_putchar('\n');
+	}
 }
