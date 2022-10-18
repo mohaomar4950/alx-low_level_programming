d1f6ae5debadce6200ab25783b9d0e335d7e472a
Mohammad Omar <mohaomar4950@gmail.com>
2022-10-18 03:42:46 -0700

    prints alphabets without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ * Description: printsalphabets without q and e.
+ * Author: mohaomar4950
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+			a++;
+	}
+	putchar(10);
+	return (0);
+}
