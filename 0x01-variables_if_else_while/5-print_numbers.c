39be0157dcc35dcb30f78b8553f1d3ffe1e4ad9b
Mohammad Omar <mohaomar4950@gmail.com>
2022-10-18 04:28:36 -0700

    prints numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ * Description: prints numbers 0-9.
+ * Author: mohaomar4950
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
