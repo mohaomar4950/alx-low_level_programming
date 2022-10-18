4e4330a90cf345c622c97ae0b1f068094b0a7b4b
Mohammad Omar <mohaomar4950@gmail.com>
2022-10-18 05:59:58 -0700

    printcombination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	int c = 48;
 
-	while (c <= 57)
+	while (c < 58)
 	{
+		putchar(c);
 		if (c != 57)
 		{
-			putchar(c);
 			putchar(44);
 			putchar(32);
 		}
 		c++;
 	}
-	putchar(57);
+	putchar(10);
 	return (0);
 }
