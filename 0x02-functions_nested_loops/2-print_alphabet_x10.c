0c4bbef4ebb5761fc6bd12b39ae1f7aba3a83bc8
Mohammad Omar <mohaomar4950@gmail.com>
2022-10-19 16:08:33 -0700

    prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - void
+ * Description: prints alphabets  for 10 timnes.
+ * Author: mohaomar4950
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 97;
+	int count = 1;
+
+	while (count < 10)
+	{
+		while (c <= 122)
+		{
+			_putchar(c);
+			c++;
+		}
+		count++;
+		_putchar(10);
+	}
+}
