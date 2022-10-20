721b1a052b299858f2f04551ffa73bdb97953b94
Mohammad Omar <mohaomar4950@gmail.com>
2022-10-20 03:02:13 -0700

    prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,13 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	_putchar(last_digit);
+	if (n < 0)
+	{
+		last_digit *= -1;
+	}
+	int ld = last_digit;
+
+	_putchar(ld);
 
 	return (last_digit);
 }
