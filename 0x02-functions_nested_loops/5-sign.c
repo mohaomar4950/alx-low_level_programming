9088f545fef1cf5d81db6f1ff966625ce3951b52
Mohammad Omar <mohaomar4950@gmail.com>
2022-10-20 02:16:47 -0700

    checks sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,22 +14,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else
 	{
 		_putchar('_');
-		_putchar(',');
-		_putchar(' ');
 		return(-1);
 	}
 }
